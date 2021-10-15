45a5aa8edd53a51ed967303b49783a0fecfc8ebb
laruh <oblomoff616@gmail.com>
2021-10-15 23:26:36 +0700

    3. Перегрузить метод max для трех чисел [#122644]

---
+++ src/main/java/ru/job4j/oop/Max.java
@@ -0,0 +1,24 @@
+package ru.job4j.oop;
+
+public class Max {
+    public int max(int first, int second) {
+        return first < second ? second : first;
+    }
+
+    public int max(int first, int second, int third) {
+        return max(
+                first,
+                max(second, third)
+        );
+    }
+
+    public int max(int first, int second, int third, int fourth) {
+        return max(
+                first,
+                max(second, max(
+                        third,
+                        fourth)
+                )
+        );
+    }
+}
---
+++ src/test/java/ru/job4j/oop/MaxTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MaxTest {
+
+    @Test
+    public void testMax() {
+        Max max = new Max();
+        int rsl = max.max(1, 2);
+        assertThat(rsl, is(2));
+    }
+
+    @Test
+    public void testMax1() {
+        Max max = new Max();
+        int rsl = max.max(3,8,0);
+        assertThat(rsl, is(8));
+    }
+
+    @Test
+    public void testMax2() {
+        Max max = new Max();
+        int rsl = max.max(18, 8, 4, 3);
+        assertThat(rsl, is(18));
+    }
+}
