f937eade32de95eb6b7fc278d5a920472c3ed385
laruh <oblomoff616@gmail.com>
2021-10-16 00:56:38 +0700

    4. Расстояние между точками в трехмерном пространстве. [#122643]

---
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.oop;
+
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class PointTest {
+
+    @Test
+    public void distance() {
+        Point a = new Point(0, 1);
+        Point b = new Point(0, 2);
+        double rsl = a.distance(b);
+        assertThat(rsl, closeTo(1, 0.001));
+    }
+
+    @Test
+    public void distance3d() {
+        Point a = new Point(0, 1, 0);
+        Point b = new Point(0, 1, 2);
+        double rsl = a.distance3d(b);
+        assertThat(rsl, closeTo(2, 0.0001));
+    }
+}
