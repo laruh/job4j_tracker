db8f1632ee47bc39b27553a8571fdd1feac6aad1
laruh <oblomoff616@gmail.com>
2021-10-19 20:33:17 +0700

    4. Переопределение [#174890]

--- src/main/java/ru/job4j/inheritance/TextReport.java
+++ src/main/java/ru/job4j/inheritance/TextReport.java
@@ -2,6 +2,8 @@ package ru.job4j.inheritance;
 
 public class TextReport {
     public String generate(String name, String body) {
-        return name + System.lineSeparator() + body;
+        return name
+                + System.lineSeparator()
+                + body;
     }
 }
