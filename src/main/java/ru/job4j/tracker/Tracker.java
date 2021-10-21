61c3eda49c57062f8cef47f1ecfb70df585b77d7
laruh <oblomoff616@gmail.com>
2021-10-21 21:35:43 +0700

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -63,6 +63,8 @@ public class Tracker {
             item.setId(id);
             items[index] = item;
             return true;
-        } else return false;
+        } else {
+            return false;
+        }
     }
 }
