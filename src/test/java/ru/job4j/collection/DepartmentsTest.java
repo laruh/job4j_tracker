14308f0664f749a6783f5328df1a104ee23b36ca
laruh <oblomoff616@gmail.com>
2021-11-12 01:09:27 +0700

    2. Отсортировать департаменты [#34352]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -30,4 +30,20 @@ public class DepartmentsTest {
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void sortDesc() {
+        List<String> input = Arrays.asList("k1", "k2/sk2", "k1/sk1/ssk2", "k1/sk1", "k2");
+        List<String> expect = Arrays.asList("k2", "k2/sk2", "k1", "k1/sk1", "k1/sk1/ssk2");
+        Departments.sortDesc(input);
+        assertThat(input, is(expect));
+    }
+
+    @Test
+    public void sortAsc() {
+        List<String> input = Arrays.asList("k1", "k2/sk2", "k1/sk1/ssk2", "k1/sk1", "k2");
+        List<String> expect = Arrays.asList("k1", "k1/sk1", "k1/sk1/ssk2", "k2", "k2/sk2");
+        Departments.sortAsc(input);
+        assertThat(input, is(expect));
+    }
 }
