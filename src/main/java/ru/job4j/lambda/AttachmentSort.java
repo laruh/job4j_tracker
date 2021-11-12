0062e8b73436404b2d05d7a90a14d85bdf028cd3
laruh <oblomoff616@gmail.com>
2021-11-12 23:53:39 +0700

    1.1. Анонимные классы [#214142]

---
+++ src/main/java/ru/job4j/lambda/AttachmentSort.java
@@ -0,0 +1,43 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
+
+public class AttachmentSort {
+    public static void main(String[] args) {
+        List<Attachment> attachments = Arrays.asList(
+                new Attachment("image 1", 100),
+                new Attachment("image 2", 34),
+                new Attachment("image 3", 13)
+        );
+        Comparator<Attachment> comparatorSize =  new Comparator<Attachment>() {
+            @Override
+            public int compare(Attachment left, Attachment right) {
+                return left.getSize() - right.getSize();
+            }
+        };
+        attachments.sort(comparatorSize);
+        System.out.println(attachments);
+
+        ArrayList<Integer> list = new ArrayList<Integer>() {
+            @Override
+            public boolean add(Integer o) {
+                System.out.println("Add a new element to list: " + o);
+                return super.add(o);
+            }
+        };
+        list.add(100500);
+        System.out.println(list);
+
+        Comparator<Attachment> comparatorName = new Comparator<Attachment>() {
+            @Override
+            public int compare(Attachment left, Attachment right) {
+                return left.getName().compareTo(right.getName());
+            }
+        };
+        attachments.sort(comparatorName);
+        System.out.println(attachments);
+    }
+}
